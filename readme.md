f102882cf23355bf59cea5cde0e1da3c3ef6202c
mmamorim <marc.m.amorim@gmail.com>
2022-10-03 20:44:01 -0300

    versão antes do recreio

--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 # Aula 08 - SDM - Segunda NOITE 03/10
 
 descrição em breve....
+
+```
+sgdb.db.jogos['teste'] = {
+            "id": "teste",
+            "nome": "Teste",
+            "produtora": "",
+            "tipo": ""
+        }
+await sgdb.write()
+console.log(sgdb.db);
+```
--- src/server.js
+++ src/server.js
@@ -1,2 +1,20 @@
+import sgdb from '../database/sgdb.js'
+import express from 'express'
 
-console.log('oi gente');
+await sgdb.init()
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('🚒 Hello World')
+})
+
+app.get('/jogos', function (req, res) {
+  res.json(sgdb.db.jogos)
+})
+
+
+app.listen(3000, () => {
+    console.log('🔥 estou escutando na porta 3000');
+})
+
